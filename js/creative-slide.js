5b4bf3343c9bbb0c58f14f1590e262ebd82b4006
Galileo Guzman <guzgali@gmail.com>
2013-11-11 01:32:16 -0600

    Algunos
    Terminando los estilos

--- js/creative-slide.js
+++ js/creative-slide.js
@@ -1,15 +1,47 @@
 $(document).on('ready', slider);
 
-	function slider(){
-		var pauseTime = 10000;
+function slider(){
+	var pauseTime = 5000;
+	
+	$('#slider .slide:gt(0)').hide();
 
-		$('#slider .slide:gt(0)').hide();
 
-	    setInterval(function(){
-	      	$('#slider .slide:first-child').fadeOut(0)
-	         .next('.slide').fadeIn(200)
-	         .end().appendTo('#slider');
-	     },pauseTime);
+	setInterval(function(){
+
+		var $current = $('#slider .slide:first-child');
+
+		slide_animation($current);
+
+		//$('#slide-options a').click(slide_direction($current));
+
+
+
+		
+		$('.next').click(function(){
+			$current.fadeOut(0);
+		    $current.next().fadeIn(50).end().appendTo('#slider');
+		    return false;
+		});
+
+
+		
+
+	},pauseTime);
+
+	function prev(){
+			$current.fadeOut(0);
+		    $current.next().fadeIn(50).appendTo('#slider');
 	}
+}
 
+function slide_animation($current){
+
+	$current.fadeOut(100)
+	.next('.slide').fadeIn(100)
+	.end().appendTo('#slider');
+
+}
+
+function slide_direction($current){
+	console.log('hey');
 }
